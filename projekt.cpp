19f259713b0e877e41d1256ed3e52b09cb3b9316
Kakihino <56163225+Kakihino@users.noreply.github.com>
2019-12-06 15:53:41 +0100

    Update projekt.cpp

--- projekt.cpp
+++ projekt.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 using namespace std;
 
 int main()
 {
+	/*
+	vector to dynamiczna tablica
+	inicjacja: vector<int>;
+	*/
+	
 	/*
 	f(x)=(sin(x/10)+1)/2
 	*/
